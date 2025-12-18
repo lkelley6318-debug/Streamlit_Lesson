8ad0f849da154aa692ddb4f12768a973c435668c
lkelley6318-debug <lkelley6318@email.vccs.edu>
2025-12-18 17:44:58 -0500

    Update streamlit-bot.py

--- streamlit-bot.py
+++ streamlit-bot.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 prompt = st.chat_input("Say something")
 if prompt:
-    st.write(f"User has sent the following promot:{prompt}")
+    st.write(f"User has sent the following prompt: {prompt}")
