7e87fdae597f5fe2647f21c57ad77d69b4edcdd8
lkelley6318-debug <lkelley6318@email.vccs.edu>
2025-12-18 17:40:25 -0500

    Update streamlit-bot.py

--- streamlit-bot.py
+++ streamlit-bot.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import numpy as np
 
-prompt = st.chat_input("Say something")
-if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+with st.chat_message("assistant"):
+    st.write("Hello Human!")
+    st.bar_char(np.random.randn(30,3))
+
+
